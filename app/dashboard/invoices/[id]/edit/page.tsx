313b2302d0271118026d382ee14ea307434666fa
Tanaka Rikuto <ludengt@gmail.com>
2026-05-25 21:56:31 +0900

    13章練習前

--- app/dashboard/invoices/[id]/edit/page.tsx
+++ app/dashboard/invoices/[id]/edit/page.tsx
@@ -2,6 +2,7 @@ import Form from '@/app/ui/invoices/edit-form';
 import Breadcrumbs from '@/app/ui/invoices/breadcrumbs';
 import { parseAppSegmentConfig } from 'next/dist/build/segment-config/app/app-segment-config';
 import { fetchInvoiceById, fetchCustomers } from '@/app/lib/data';
+import { notFound } from 'next/navigation';
  
 export default async function Page(props: { params: Promise<{id: string }> }){
 const params = await props.params;
@@ -10,6 +11,10 @@ const [invoice, customers] = await Promise.all([
     fetchInvoiceById(id),
     fetchCustomers(),
 ])
+
+if (!invoice) {
+    notFound();
+}
 
   return (
     <main>
