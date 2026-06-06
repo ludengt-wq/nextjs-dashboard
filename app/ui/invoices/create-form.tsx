09d6a0b6e53236f235ac6ad33f8dd1f7476ce86e
Tanaka Rikuto <ludengt@gmail.com>
2026-06-07 01:05:02 +0900

    13章完了

--- app/ui/invoices/create-form.tsx
+++ app/ui/invoices/create-form.tsx
@@ -45,13 +45,13 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             <UserCircleIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500" />
           </div>
           <div id="customer-error" aria-live="polite" aria-atomic="true">
-        {state.errors?.customerId &&
-          state.errors.customerId.map((error: string) => (
+            {state.errors?.customerId &&
+            state.errors.customerId.map((error: string) => (
             <p className="mt-2 text-sm text-red-500" key={error}>
               {error}
             </p>
-          ))}
-      </div>
+            ))}
+          </div>
         </div>
 
         {/* Invoice Amount */}
@@ -72,6 +72,14 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
                 //required
               />
               <CurrencyDollarIcon className="pointer-events-none absolute left-3 top-1/2 h-[18px] w-[18px] -translate-y-1/2 text-gray-500 peer-focus:text-gray-900" />
+            </div>
+            <div id="customer-error" aria-live="polite" aria-atomic="true">
+            {state.errors?.amount &&
+            state.errors.amount.map((error: string) => (
+            <p className="mt-2 text-sm text-red-500" key={error}>
+              {error}
+            </p>
+            ))}
             </div>
           </div>
         </div>
@@ -117,6 +125,19 @@ export default function Form({ customers }: { customers: CustomerField[] }) {
             </div>
           </div>
         </fieldset>
+        <div id="customer-error" aria-live="polite" aria-atomic="true">
+          {state.errors?.status &&
+          state.errors.status.map((error: string) => (
+          <p className="mt-2 text-sm text-red-500" key={error}>
+            {error}
+          </p>
+          ))}
+        </div>
+        <div id="customer-error" aria-live="polite" aria-atomic="true">
+          {state.message && (
+            <p className="mt-2 text-sm text-red-500">{state.message}</p>
+          )}
+        </div>
       </div>
       <div className="mt-6 flex justify-end gap-4">
         <Link
